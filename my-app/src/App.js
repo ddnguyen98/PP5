ee3b6b67851b41ed9391b3db1e4a666a2fbfcfb1
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2019-07-18 08:45:53 -0400

    Milestone 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,68 +6,116 @@ class App extends Component{
   state ={
     uinfo: [],
     dog: [],
+    dogName: '',
+    dogTemp:[],
+    dogid: '',
+    dogimg: null,
     isLoadingInfo: false,
     isLoadingDog: false
-  }
+    }
 
 
-  loadApi(){
+  loadApi = e =>{
+
+    console.log('loadApi')
     if(this.state.isLoadingInfo === false && this.state.isLoadingDog === false){
+      console.log('loadperson')
       this.loadDetailsPerson();
      }
     else if(this.state.isLoadingInfo === true && this.state.isLoadingDog === false){
+      console.log('loadDog')
       this.loadDetailsDog();
     }
+
   }
 
-  loadDetailsPerson(){
+  loadDetailsDname = e =>{
+    console.log('loadgname')
+    fetch('https://randomuser.me/api/')
+    .then(response => { return response.json() })
+    .then(data => {this.setState({dogName : data.results[0].name.first})})
+  }
+
+  loadDetailsPerson = e =>{
     fetch('https://randomuser.me/api/')
     .then(response => { return response.json() })
     .then(data => {
       let reg =/^[a-zA-Z]+$/;
-      if(!reg.test(data.results[0].name.first.toLowerCase())){
-        this.loadDetailsPerson();
+      if(!reg.test(data.results[0].name.first)){
+        console.log('Error');
+        this.loadApi();
       }
-      this.setState({uinfo : data.results[0], isLoadingInfo: true})})
+      else{
+        this.setState({uinfo : data.results[0], isLoadingInfo: true}) 
+        this.loadDetailsDname();
+      }
+    })
   }
 
-  loadDetailsDog(){
+  loadDetailsDog = e =>{
     fetch(`https://api.thedogapi.com/v1/breeds/search?q=${this.state.uinfo.name.first[0]}`)
     .then(response => { return response.json() })
     .then(data => {
       for (let i = 0; i < data.length; i++) {  
         if(data[i].name[0].toLowerCase() === this.state.uinfo.name.first[0].toLowerCase()){
           this.setState({dog : data[i], isLoadingDog: true})
+          this.setState({dogid: data[i].id})
+          this.setState({dogTemp: data[i].temperament})
+          if(typeof data[i].temperament === 'undefined'){
+            console.log('Test')
+            this.setState({dogTemp: "Happy, Loving, Kind"});
+          }
+          if(this.state.dogimg === null){
+            console.log('img')
+
+            fetch(`https://api.thedogapi.com/v1/images/search?breed_ids=${this.state.dogid}`)
+            .then(response => { return response.json() })
+            .then(info =>{
+              if(info.length === 0){
+                this.setState({dogimg: "https://cdn2.thedogapi.com/images/WIf5o1E8h.jpg"})
+              }
+              else{
+                this.setState({dogimg: info[0].url})
+              }
+            })
+          }
+
           break;
         }
       }
     })
   }
 
-  showContent(){
-
+  changeContents = e =>{
+    console.log('hi')
+    this.setState({isLoadingDog: false, isLoadingInfo: false, dogimg: null})
+    this.loadApi();
   }
+
 
   render(){
-    let { isLoadingInfo, isLoadingDog, uinfo, dog } = this.state
-    this.loadApi();
-
+    console.log(this.state);
+    let { isLoadingInfo, isLoadingDog, uinfo, dog, dogName, dogimg, dogTemp } = this.state
+      if(isLoadingDog === false || isLoadingInfo === false ){
+        this.loadApi();
+      }
   
-      if(isLoadingDog === false || isLoadingInfo === false){
+      if(isLoadingDog === false || isLoadingInfo === false ){
         return <div>Loading Content</div>
       }
-      else if(isLoadingDog === true || isLoadingInfo === true){
-        return (
+      else if(isLoadingDog === true && isLoadingInfo === true){
+        return(
             <div style={styles.container} id="changeme">
-            <p>{dog.name}</p>
-            <p>{uinfo.name.first}</p>
-            <p>Hello my name is {uinfo.name.first} and I am {dog.temperament}.</p>
-            <p>My owner is called {uinfo.name.title} {uinfo.name.last}.</p>
+            <img src={dogimg} alt="Dog Img" height="100" width="100"></img>
+            <p id="">{dog.name}</p>
+            <p>Hello my name is {dogName} and I am {dogTemp}.</p>
+            <p>My owner is called {uinfo.name.first} {uinfo.name.last}.</p>
             <p>You can contact my owner at {uinfo.email}.</p>
-          </div>
+            <button onClick={this.changeContents}>Test</button>
+            </div>
         );
       }  
-  }
+   }
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
 import * as serviceWorker from './serviceWorker';
+
+const config = {
+    apiKey: "AIzaSyD2bxg_sJcq3m625n7tDHAp_nkMjGtUdOw",
+    authDomain: "dogdata-3200f.firebaseapp.com",
+    databaseURL: "https://dogdata-3200f.firebaseio.com",
+    projectId: "dogdata-3200f",
+    storageBucket: "dogdata-3200f.appspot.com",
+    messagingSenderId: "1085718428729",
+    appId: "1:1085718428729:web:b95fef7c3f7b8e31"
+  };
+
+firebase.initializeApp(config)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
